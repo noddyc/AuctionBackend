d38403bffe93b75f4da2c9bbb20a5b5fd7fb33bc
jianhe2git <59978308+noddyc@users.noreply.github.com>
2023-02-09 06:59:34 -0500

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.json())
 // app.use(cookieParser())
 app.use(
     cors({
-      origin: ["http://localhost:3000","http://localhost:3001", "https://dancing-bienenstitch-6e1606.netlify.app"],
+      origin: ["http://localhost:3000","http://localhost:3001", "http://dancing-bienenstitch-6e1606.netlify.app"],
       credentials: true,
     })
   );
