7d8de675b35f82526adb95ba60fbb3968db7c11e
jianhe2git <59978308+noddyc@users.noreply.github.com>
2023-02-10 03:30:23 -0500

    1

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.json())
 // app.use(cookieParser())
 app.use(
     cors({
-      origin: ["http://52.87.235.156:3000"],
+      origin: ["http://52.87.235.156:3000", "http://localhost:3000"],
       credentials: true,
     })
   );
