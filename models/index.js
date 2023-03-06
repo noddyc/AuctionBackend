d6d79ce914744479326d14c9b67557c42f94fc04
jianhe2git <59978308+noddyc@users.noreply.github.com>
2023-03-06 06:32:50 -0500

    1

--- models/index.js
+++ models/index.js
@@ -66,8 +66,6 @@ db.notification.belongsTo(db.auction, {foreignKey: 'auctionId', onDelete: 'casca
 db.notification.belongsTo(db.user, {foreignKey:'senderId', onDelete: 'cascade'})
 db.notification.belongsTo(db.user, {foreignKey:'receiverId', onDelete: 'cascade'})
 
-// db.image.belongsTo(db.auction, {as:'i_Id', through: db.image_auction, foreignKey: 'aId'})
-// db.auction.belongsToMany(db.image,{as: 'a_Id', through: db.image_auction, foreignKey: 'id'})
 db.image.belongsTo(db.auction, {foreignKey: 'auctionId',onDelete:'cascade'})
 
 
@@ -131,7 +129,7 @@ function generate(){
     let price_arr = ['212.02', '2132.12', '65.90', '434.12', '769.03']
     
     // moment(year+'-'+getRandomInt(2,9)+'-'+getRandomInt(1,25)+' '+getRandomInt(1,20)+':'+getRandomInt(1,50)+':00', 'UTC').format()
-    for(let i = 0; i < 15; i++){
+    for(let i = 0; i < 100; i++){
         let product_name = uniqueNamesGenerator({ dictionaries: [animals] });
         let product_description =  uniqueNamesGenerator({
             dictionaries: [adjectives, colors, adjectives, colors, adjectives, colors, 
