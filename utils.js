1ee307fa94a7f229073e78560738c402ef625325
jianhe2git <59978308+noddyc@users.noreply.github.com>
2023-03-16 08:40:10 -0400

    1

--- utils.js
+++ utils.js
@@ -1,8 +1,8 @@
-let dayHour = 11; // utc
+let dayHour = 8; // should put in est time according to utc
 let dayMin = 45; // utc
 let daySec = 40; // utc
 
-// need to convert to utc
+// need to convert to utc or not
 let nightHour = 21;
 let nightMin = 17;
 let nightSec = 40;
